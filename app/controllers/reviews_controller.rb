a8691a6daee5d28771f06f97a76bdbc702b98166
Hernan Romero <hernrome@gmail.com>
2020-01-21 21:30:10 +0000

    Added controllers and started with basic views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,44 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
+  # def index
+  #   @reviews = Review.all
+  # end
+
+  # def show
+  # end
+
+  def new
+    @review = Review.new(review_params)
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  # def edit
+  # end
+
+  # def update
+  #   @review.update(review_params)
+  # end
+
+  # def destroy
+  #   @review.destroy
+  # end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,4 @@ Rails.application.routes.draw do
   resources :restaurants, only: %i[index show new create] do
     resources :reviews, only: %i[new create]
   end
-  namespace :admin do
-    resources :restaurants
-  end
 end
