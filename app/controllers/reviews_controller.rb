81f612a2135abe77b010c0596cf3733f8119b2ba
Hernan Romero <hernrome@gmail.com>
2020-01-23 18:54:47 +0000

    Added reviews to show pages. Can create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,11 +9,12 @@ class ReviewsController < ApplicationController
   # end
 
   def new
-    @review = Review.new(review_params)
+    @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
@@ -39,6 +40,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
